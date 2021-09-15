6ca2c7b59f8d0540712badcdc2699d28cb32a0f7
Matt Schouten <matt.schouten@gmail.com>
2021-09-15 07:16:18 -0500

    Allow chores to be checked off

--- chorelist/app/controllers/chores_controller.rb
+++ chorelist/app/controllers/chores_controller.rb
@@ -5,6 +5,16 @@ class ChoresController < ApplicationController
         redirect_to chore_list_path(@chore_list)
     end
 
+    def update
+        @chore_list = ChoreList.find(params[:chore_list_id])
+        @chore = Chore.find(params[:id])
+
+        chore_is_done = params.key?("completed") && params["completed"].key?(@chore.id.to_s)
+        @chore.update(is_done: chore_is_done)
+
+        redirect_to chore_list_path(@chore_list)
+    end
+
     private
         def chore_params
             params.require(:chore).permit(:name, :assigned_to, :is_done)
