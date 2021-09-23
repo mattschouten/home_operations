e6ce134fb589136672d081ff319d8fcc0ea98eab
Matt Schouten <matt.schouten@gmail.com>
2021-09-23 14:29:30 -0500

    Protect pages from users that are not logged in

--- chorelist/app/controllers/application_controller.rb
+++ chorelist/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
     helper_method :current_user_session, :current_user
+    before_action :require_user
 
     private
       def current_user_session
@@ -11,6 +12,14 @@ class ApplicationController < ActionController::Base
         return @current_user if defined?(@current_user)
         @current_user = current_user_session && current_user_session.user
       end
+
+    def require_user
+      unless current_user
+        flash[:error] = "You must be logged in to access this page"
+        redirect_to root_url
+        return false
+      end
+    end
 
     protected
         def handle_unverified_request
