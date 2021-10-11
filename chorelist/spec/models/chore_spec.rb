b29d6c6cf4fec7a9e63028cf920534d438e04113
Matt Schouten <matt.schouten@gmail.com>
2021-10-11 06:59:52 -0500

    Add RSpec to project

---
+++ chorelist/spec/models/chore_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Chore, :type => :model do
+    it "requires a name"
+    it "must be assigned"
+    it "can be done"
+    it "must belong to a chore list"
+end
