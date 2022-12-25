d0683aa097709208b48db82e16ae5e5feab42445
Matt Schouten <matt.schouten@gmail.com>
2022-12-24 20:32:09 -0600

    Make the Dockerized version work better on raw systems

--- chorelist/docker/run-server-in-docker.sh
+++ chorelist/docker/run-server-in-docker.sh
@@ -5,7 +5,14 @@ set euo pipefail
 cd ${INSTALL_PATH}
 
 bundle install
+echo "CHECKING FOR MASTER KEY in ${INSTALL_PATH}/config/master.key"
+if [[ ! -f "${INSTALL_PATH}/config/master.key" ]]; then
+    rm "${INSTALL_PATH}/config.credentials.yml.enc"
+    echo "Creating new master key"
+    EDITOR=true bin/rails credentials:edit > /dev/null
+fi
 bin/rails db:prepare
+
+echo "Precompiling assets.  RAILS_ENV=${RAILS_ENV}"
 bin/rails assets:precompile
-# bin/rails server -e production -p 80
 bin/rails server -e production -p 80
